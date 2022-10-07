db9ff0e4ae07d73d067cf86d939c16324d0967e5
Ritwika Chakraborty <ritwika.chakraborty2014@gmail.com>
2022-10-07 22:14:19 +0530

    Initial Commit

---
+++ JS/script.js
@@ -0,0 +1,37 @@
+function displace_banner() {
+    let header = document.getElementById("head");
+    let height = header.clientHeight;
+    console.log(height);
+    let bann = document.getElementById("bann");
+    bann.style.marginTop = (height + "px");
+}
+
+displace_banner();
+
+(function () {
+    var elements;
+    var windowHeight;
+
+    function init() {
+        elements = document.querySelectorAll('.hidden');
+        windowHeight = window.innerHeight;
+    }
+
+    function checkPosition() {
+        for (var i = 0; i < elements.length; i++) {
+            var element = elements[i];
+            var positionFromTop = elements[i].getBoundingClientRect().top;
+
+            if (positionFromTop - windowHeight <= 0) {
+                element.classList.add('fade-in-element');
+                element.classList.remove('hidden');
+            }
+        }
+    }
+
+    window.addEventListener('scroll', checkPosition);
+    window.addEventListener('resize', init);
+
+    init();
+    checkPosition();
+})();
